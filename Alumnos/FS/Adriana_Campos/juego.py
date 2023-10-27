5ba4591f50dccac348e1f28ab2a536d9c50cffda
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2023-10-27 19:46:22 +0200

    save

--- Alumnos/FS/Adriana_Campos/juego.py
+++ Alumnos/FS/Adriana_Campos/juego.py
@@ -1,19 +1,13 @@
-#Juego
-#elegir palabras 
 import pandas as pd
 import string
 import time
 
-
 listaPalabras= ['remedio', 'pronunciar', 'manejar', 'ley','elefante']
-#listaPalabras = pd.read_csv("/Users/adrianacamposnarvaez/Documents/GitHub/EDEM_MDA2324/Alumnos/FS/Adriana_Campos/palabras.txt")
 
-#abecedario = ['a','e','o','u','b','i','c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'ñ']
-abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','ñ']
+abecedario = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z','ñ']
 
 intento = 0
 coicide = 0
-
 inicio = time.time() 
 
 def contar_letras_unicas(palabra):
