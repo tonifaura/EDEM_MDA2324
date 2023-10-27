255b1cfa4f0ebaf66b43af375866b1284f3d0206
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2023-10-27 19:21:49 +0200

    save

--- Alumnos/FS/Adriana_Campos/juego.py
+++ Alumnos/FS/Adriana_Campos/juego.py
@@ -1,14 +1,20 @@
 #Juego
 #elegir palabras 
 import pandas as pd
+import string
+import time
+
 
 listaPalabras= ['remedio', 'pronunciar', 'manejar', 'ley','elefante']
-#listaPalabras= pd.read_csv('/Users/adrianacamposnarvaez/Documents/GitHub/EDEM_MDA2324/Alumnos/FS/#Adriana_Campos/palabras.txt')
+#listaPalabras = pd.read_csv("/Users/adrianacamposnarvaez/Documents/GitHub/EDEM_MDA2324/Alumnos/FS/Adriana_Campos/palabras.txt")
 
-import string
-abecedario = string.ascii_lowercase
+#abecedario = ['a','e','o','u','b','i','c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'ñ']
+abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','ñ']
+
 intento = 0
 coicide = 0
+
+inicio = time.time() 
 
 def contar_letras_unicas(palabra):
     return len(set(palabra))
@@ -21,8 +27,12 @@ for palabra in listaPalabras:
            intento = intento + 1 
            coicide = coicide + 1
         else:
-           intento = intento+1
+           intento = intento + 1
     
 
+fin = time.time() 
+tiempo_transcurrido = fin - inicio
+
+print (tiempo_transcurrido)
 print(intento)
 
