3a53e05c404e88fb4a505a67f351827198932899
Pedro Nieto <pedro.nieto@onlyfy.com>
2023-10-29 17:04:51 +0100

    adding

--- .github/code/check.py
+++ .github/code/check.py
@@ -27,8 +27,7 @@ def generate_table(alumnos):
         table="<table><tr><th>Alumno</th>"
         for element in deliverables:
             table+="<th>"+element+"</th>"
-        table+="</tr>"
-        
+        table+="</tr>"  
         for alumno in alumnos:
             table+="<tr><td>"+str.capitalize(alumno)+"</td>"
             for element in deliverables:
@@ -46,16 +45,22 @@ def modify_readme():
     with open('README.md', 'r') as file:
         data = file.read()
         parts = data.split('### Estado de las entregas')
+
     with open('README.md', 'w') as file:
-        file.write(parts[0])
-        file.write('### Estado de las entregas\n')
-        file.write('Entregas Entre Semana\n')
-        file.write(generate_table(check_class('Alumnos/FS')))
-        file.write('\n')
-        file.write('\n')
-        file.write('Entregas Entre Semana\n')
-        file.write(generate_table(check_class('Alumnos/FS')))
-        file.write('\n')
+        try: 
+            file.write(parts[0])
+            file.write('### Estado de las entregas\n')
+            file.write('Entregas Fin de Semana\n')
+            file.write(generate_table(check_class('Alumnos/FS')))
+            file.write('\n')
+            file.write('\n')
+            file.write('Entregas Entre Semana\n')
+            file.write(generate_table(check_class('Alumnos/ES')))
+            file.write('\n')
+        except:
+            file.close()
+            with open('README.md', 'w') as file_recov:
+                file_recov.write(data)
         
 
 
@@ -63,6 +68,5 @@ def modify_readme():
 if __name__ == '__main__':
     
     #check_class('Alumnos/FS')
-    print(generate_table(check_class('Alumnos/ES')))
     modify_readme()
 
