a4a04816fd669b72569d4d86118289616f159c9c
Jose Antonio Rodrigo Cortes <josanrc.rcsl@gmail.com>
2023-10-31 12:10:16 +0100

    Update README.sh

--- Alumnos/ES/Josan_Rodrigo_Cortes/Linux/Ejercicio_Comandos_Linux/README.sh
+++ Alumnos/ES/Josan_Rodrigo_Cortes/Linux/Ejercicio_Comandos_Linux/README.sh
@@ -8,34 +8,35 @@
 echo "Ejercicio de comandos en la consola de linux."
 
   echo "1.Listar todos los archivos del directorio bin."
-  ls -a bin
+  ls -a /bin
 
   echo "2.Listar todos los archivos del directorio tmp."
-  ls tmp -a
+  ls /tmp
     
   echo "3.Listar todos los archivos del directorio etc que empiecen por t."
-  ls -a /etc/t*
+  ls /etc/t*
 
   echo "4.Listar todos los archivos del directorio dev que empiecen por tty."
-  ls -a /dev/tty*
+  ls /dev/tty*
     
   echo "5.Listar todos los archivos del directorio dev que empiecen por tty y acaben en 3." 
-  ls -a tty*3
+  ls -a /dev/tty*[3]
   # Hay alguno??
   echo "6.Listar todos los archivos del directorio dev que empiecen por t y acaben en C1."
   ls -a /dev/t*c1
   # Hay alguno?
   echo "7.Listar todos los archivos, incluidos los ocultos, del directorio raíz."
-  ls -a
+  ls -a/
 
   echo "8.Listar todos los archivos del directorio etc que no empiecen por t."
   ls -d /etc/[^t]*
   
   echo "9.Listar todos los archivos del directorio usr y sus subdirectorios."
-  ls /usr *
+  ls -R /usr
+  #ls /usr * Podría valer?
 
   echo "10.Cambiarse al directorio tmp, crear directorio PRUEBA."
-  mkdir /tmp/PRUEBA
+  cd /tmp && mkdir /tmp/PRUEBA
     
   echo "11.Verificar que el directorio actual ha cambiado."
   pwd
@@ -44,14 +45,13 @@ echo "Ejercicio de comandos en la consola de linux."
   date
 
   echo "13.Con un solo comando posicionarse en el directorio $HOME."
-  cd /$HOME
+  cd /
  
   echo "14.Verificar que se está en él."
   pwd
 
   echo "15.Listar todos los ficheros del directorio HOME mostrando sus permisos."
-  ls -al $HOME #opcion1
-  ls -l $HOME # opcion2
+  ls -al /
 
   echo "16.Borrar todos los archivos y directorios visibles de vuestro directorio PRUEBA."
   rmdir /tmp/PRUEBA
@@ -74,8 +74,15 @@ echo "Ejercicio de comandos en la consola de linux."
   cp mensaje.txt /tmp/PRUEBA/dir2
   cp mensaje.txt /tmp/PRUEBA/dir3
 
-  # 20.Comprobar el ejercicio anterior mediante un solo comando.
-    
+  Echo "20.Comprobar el ejercicio anterior mediante un solo comando."
+  
+  ls -R /etc
 
-    
-   
+#Para crear un script bash en Linux, sigue estos pasos:
+#Abre tu editor de texto preferido (por ejemplo, vim, gedit o nano).
+#Escribe tus comandos en el archivo, uno debajo del otro. Asegúrate de que cada comando esté en una nueva línea.
+#Guarda el archivo con un nombre que termina en ".sh" (por ejemplo, "myscript.sh").
+#Abre el terminal y navega hasta la carpeta donde has guardado el archivo del script.
+#Darle permisos de ejecución al script con el comando "chmod +x nombre_del_script.sh".
+#Ejecuta el script con el comando "./nombre_del_script.sh".
+#https://como.help/linux/ubuntu/como-crear-y-ejecutar-un-script-bash-en-linux-con-ejemplos
