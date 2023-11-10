09628bc42a40abad828f711ec562e9a27d23a9f3
Luseca <148787198+Luseca@users.noreply.github.com>
2023-11-10 10:52:09 +0100

    ProvisionalCHUCKLuisS

--- Alumnos/ES/Luis_Segura/CHUCK/results/chuck.py
+++ Alumnos/ES/Luis_Segura/CHUCK/results/chuck.py
@@ -1,25 +1,25 @@
 print("Hola mundo")
 
+
 import requests
 import time
 
 url = 'https://api.chucknorris.io/jokes/random'
 response = requests.get(url)
 
-if response.status_code == 200:
+import requests
+import time
 
-    data = response.json()
-    print(data['value'])
-
-else:
-    print(f'Error en la solicitud. Código de estado: {response.status_code}')
+url = 'https://api.chucknorris.io/jokes/random'
+bromas = ""
 try:
     while True:
         response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
-            print(data['value'])
+            bromas = bromas+data['value']+"\n"
+            print(bromas)
         else:
             print(f'Error en la solicitud. Código de estado: {response.status_code}')
 
@@ -27,3 +27,7 @@ try:
 
 except KeyboardInterrupt:
         print("¡Bucle interrumpido por el usuario!")
+
+with open("results.txt", "a") as f:
+    
+    f.write(bromas+ "\n")
