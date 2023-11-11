175dc330a15ac2716f82ee373d16e3dc4e741e0c
albertodegeapla <146119880+albertodegeapla@users.noreply.github.com>
2023-11-11 08:57:28 +0100

    proyecto final
    inicio proyecto final

---
+++ Alumnos/FS/ALBERTO_DE_GEA/Python/PROYECTO FINAL/proyecto-final.py
@@ -0,0 +1,16 @@
+# Una empresa de formación quiere gestionar su cartera de alumnos. 
+
+#Escribe un programa que guarde una lista de alumnos. Cada Alumno dispone de los siguientes campos:
+
+#- NIF (string)
+#- Nombre (string)
+#- Apellidos (string)
+#- Teléfono (string)
+#- Email (string)
+#- Aprobado (boolean)
+
+listaAlumnos = [
+    {'NIF': "21700504G", 'nombre': "alberto", 'apellidos': "de gea", 'teléfono': 689628823, 'email': "albertopla1966@gmail.com", 'aprobado': True}
+    ]
+
+print(listaAlumnos)
