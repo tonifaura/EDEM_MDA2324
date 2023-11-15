1f30583209724da0a2d535e1864e05337c62bb68
pepe54aguilar <pep343000@gmail.com>
2023-11-15 19:12:33 +0100

    update py
    el programa ya cuenta los sustantivos independientemente del nº chistes

--- Alumnos/FS/Pepe_Aguilar_van_der_Hofstadt/Chuck/chuck.py
+++ Alumnos/FS/Pepe_Aguilar_van_der_Hofstadt/Chuck/chuck.py
@@ -15,9 +15,10 @@ class Sustantivo:
     def reset(self):
         self.veces = 0
 
-lista_sustantivos = []
 
-def random_chuck_norris_joke(url: str) -> str:
+
+
+def random_chuck_norris_joke(url: str) -> str:      # Extrae una broma random de chuck norris
 
     respuesta = requests.get(url)
     estado: int = respuesta.status_code
@@ -28,43 +29,44 @@ def random_chuck_norris_joke(url: str) -> str:
     else:
         print(f'error {estado}')
     
-def separa_sustantivos(broma:str, todos_sustantivos) -> list:
+def separa_sustantivos(joke:str, todos_sustantivos) -> list:       # separa los sustantivos del resto del chiste
     sustantivos = []
-    broma_separada = broma.split()
+    broma_separada = joke.split()
     for palabra in broma_separada:
         for sust in todos_sustantivos:
-            if(palabra.lower() == sust):
-                if(len(lista_sustantivos) == 0):
-                    sustantivo_nuevo = Sustantivo(palabra)
-                    sustantivo_nuevo.reset()
-                    lista_sustantivos.append(sustantivo_nuevo)
-                for repetir in lista_sustantivos:
-                    
-                    if(repetir.palabra == palabra):
-                        repetir.suma_vez()
-                    else:
-                        sustantivo_nuevo = Sustantivo(palabra)
-                        lista_sustantivos.append(sustantivo_nuevo)
-                #sustantivos.append(palabra)
+            if(palabra.lower() == sust):  
+                sustantivos.append(palabra)
     return(sustantivos)
 
+def cuenta_sustantivos(sustantivos_chiste:list):    # Mete en una lista los sustantivos y los cuenta
+    for palabra in sustantivos_chiste:
+        meter_nuevo = True
+        for sust in lista_sustantivos:
+            if(sust.palabra == palabra):
+                meter_nuevo = False
+                sust.suma_vez()
+        if(meter_nuevo == True):
+            sustantivo_nuevo = Sustantivo(palabra)
+            lista_sustantivos.append(sustantivo_nuevo)
+            
 
+             
 
 
 sustantivos_df = pd.read_csv('nounlist.csv', header=None, names=['sustantivos'])
 palabras = sustantivos_df['sustantivos'].str.strip('"').tolist()
 
+lista_sustantivos = []
+
 URL:str = 'https://api.chucknorris.io/jokes/random'
 
+for n in range(10):
+    broma = random_chuck_norris_joke(URL)
+    sustativos = separa_sustantivos(broma, palabras)
+    cuenta_sustantivos(sustativos)
 
-broma = random_chuck_norris_joke(URL)
-sustativos_chiste = separa_sustantivos(broma, palabras)
-
-print(broma)
 for palabras in lista_sustantivos:
     print(f'{palabras.palabra} {palabras.veces}')
-
-
 
 
 
