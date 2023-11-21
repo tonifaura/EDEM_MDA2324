b2118fcfea91cf23d6cd69f73a694b8e7744780b
jarupu <javieruizpuchol@gmail.com>
2023-11-21 12:52:24 +0100

    Actualizo el sh

---
+++ Alumnos/ES/Javier_Ruiz/Chuck/shell.sh
@@ -0,0 +1,6 @@
+echo hello world
+mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/
+mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/results/
+touch /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/results/chistes.txt
+
+/usr/bin/python3 mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/python.py
