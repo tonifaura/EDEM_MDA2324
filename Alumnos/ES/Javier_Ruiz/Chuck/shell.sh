4aa4f02fb7d1739a44d1abb7ba0e82f735405143
jarupu <javieruizpuchol@gmail.com>
2023-11-21 12:56:17 +0100

    edito un espacio

--- Alumnos/ES/Javier_Ruiz/Chuck/shell.sh
+++ Alumnos/ES/Javier_Ruiz/Chuck/shell.sh
@@ -1,6 +1,6 @@
 echo hello world
-mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/
+mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck
 mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/results/
-touch /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/results/chistes.txt
+touch mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/results/
 
 /usr/bin/python3 mkdir /Users/User/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/Javier_Ruiz/Chuck/python.py
