9762240224a762b1fd2451e219e5f790296baf49
Jose Antonio Rodrigo Cortes <josanrc.rcsl@gmail.com>
2023-11-22 15:25:48 +0100

    221123 1330

---
+++ Alumnos/ES/Josan_Rodrigo_Cortes/Python/Retos/Reto8.py
@@ -0,0 +1,9 @@
+""" Escribe un programa que pueda decirte si un número (número entero) es primo o no """
+
+numero=int(input("Introduce un número: "))
+
+
+if numero%2==0:
+    print("No es primo")
+else:
+    print("Es primo")
