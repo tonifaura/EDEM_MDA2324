70938c24b07d9cf044d76417009d1a79fb9030e5
Jose Antonio Rodrigo Cortes <josanrc.rcsl@gmail.com>
2023-11-23 12:48:31 +0100

    2311231248

--- Alumnos/ES/Josan_Rodrigo_Cortes/Retos/Reto9.py
+++ Alumnos/ES/Josan_Rodrigo_Cortes/Retos/Reto9.py
@@ -6,8 +6,8 @@ Escribe un programa que pueda decirte si un año (número entero) es bisiesto o 
 # anyo=int(input("Introduce el año a comprobar: "))
 def anyoBisiesto(anyo):
 
-    if anyo%4==0:
-        print(f"El {anyo} es bisiesto")
+    if anyo%4!=0:
+        print(f"El año {anyo} no es bisiesto")
 
     elif anyo%4==0 and anyo%100!=0:
         print(f"El año {anyo} no es bisiesto")
@@ -18,11 +18,7 @@ def anyoBisiesto(anyo):
     elif anyo%4==0 and anyo%100!=0 and anyo%400==0:
         print(f"El año {anyo} es bisiesto")
 
-anyoBisiesto(3000)
-# def añoBisiesto(anyyo):
-#     if anyyo%4 and anyyo%100 and anyyo%400==0:
-#         print("Bisiesto")
-#     else:
-#         print("No bisiesto")
-    
-# añoBisiesto(3000)
+anyyo=int(input("Introduce el año"))
+for i in range(anyyo-20,anyyo+20):
+    anyoBisiesto(i)
+
