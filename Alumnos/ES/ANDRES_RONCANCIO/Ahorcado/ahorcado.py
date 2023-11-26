bbb3cacef3de6b1718239bb5cbf330397518cacf
Gandrex87 <andresrsalamanca@gmail.com>
2023-11-26 11:53:19 +0100

    ejercicio ahorcado

--- Alumnos/ES/ANDRES_RONCANCIO/Ahorcado/ahorcado.py
+++ Alumnos/ES/ANDRES_RONCANCIO/Ahorcado/ahorcado.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+#leer archivo csv
+df =pd.read_csv('palabras.csv')
 
-with open ('palabras.csv', 'r') as archivo:
-    palabras = archivo.read().splitlines()
+#Crear lista[] 
+lista=[]
+for element in df['palabras']:
+    lista.append(element)
+print(lista)
 
+#adivinar palabra
 
-#print(palabras)
+letras_abc =('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+intentos = 0
 
-for element in palabras:
-   letras = len(element)
-  # for letras in abc:
-       
-        
-        
-        
-        
-#en el comentario pull request..poner numero de intentos150 260
-#pull request modo draf
+for i in lista:
+    letras=len(i)
+    while letras > 0:
+        for a in letras_abc:
+            intentos+=1
+            if a in i:
+                conteo = i.count(a)
+                letras-=conteo
+                if letras == 0:
+                    break
+                
+
+print(f'el numero de intentos  fue {intentos}')
