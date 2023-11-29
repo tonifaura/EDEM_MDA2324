e423e082040686a82780f36ada832a72a0e5aee7
jarupu <javieruizpuchol@gmail.com>
2023-11-30 00:25:56 +0100

    131 intentos

--- Alumnos/ES/Javier_Ruiz/Ahorcado/main.py
+++ Alumnos/ES/Javier_Ruiz/Ahorcado/main.py
@@ -1,22 +1,43 @@
 import pandas as pd
 import time
+import unidecode
+from collections import Counter
 
 # Lee el CSV
+
 df = pd.read_csv('palabras.csv')
 
-# Extrae la columna "Palabras" como una lista
-palabras_lista = df['PALABRAS'].tolist()
-palabras_lista = [palabra.lower() for palabra in palabras_lista] #las pongo en minuscula porque sino da problemas
+# Extrae la columna "Palabras" como una lista y las pongo en minuscula porque sino da problemas
 
-# Ahorcado
-letras_disponibles =  ['e', 'a', 'o', 'i', 's', 'r', 'n', 'l', 'u', 'd', 't', 'c', 'm', 'p', 'b', 'g', 'v', 'y', 'q', 'h', 'f', 'z', 'j', 'x', 'k', 'w']
+palabras_lista = df['PALABRAS'].tolist()
+palabras_lista = [palabra.lower() for palabra in palabras_lista] 
+
+# Diccionario de palabras sacado de https://github.com/JorgeDuenasLerin/diccionario-espanol-txt
+
+palabrastodas = "0_palabras_todas_no_conjugaciones.txt"
+rae_palabras = []
+
+# Limpiando el diccionario para poder usarlo
+
+with open(palabrastodas, 'r', encoding='utf-8') as archivo:
+    for linea in archivo:
+        # Eliminar los caracteres de nueva línea al final de cada línea
+        palabra = linea.strip()
+        # Quito tildes y diacríticos usando unidecode
+        palabra_sin_tildes = unidecode.unidecode(palabra)
+        # Convertir a minúsculas
+        palabra_minuscula = palabra_sin_tildes.lower()
+        # Eliminar caracteres no deseados (por ejemplo, '-')
+        palabra_limpia = ''.join(caracter for caracter in palabra_minuscula if caracter.isalnum() or caracter.isspace())
+        # Agregar las palabras limpias a la lista
+        rae_palabras.append(palabra_limpia)
+
+# AHORCADO
+
 total_intentos = 0
 iniciocrono = time.time()
 
 while palabras_lista:
-    intentos_realizados = 0
-    letras_correctas = 0
-
     palabra_seleccionada = palabras_lista[0]
     palabras_lista.remove(palabra_seleccionada)
 
@@ -24,11 +45,45 @@ while palabras_lista:
 
     numero_letras = len(palabra_seleccionada)
 
-    for letra in letras_disponibles:
-        intentos_realizados += 1     #suma 1 intento por cada letra intentada del set de letras disponible
-        if palabra_seleccionada.count(letra) > 0:           #si la letra intentada esta presente en la palabra seleccionada
-            letras_correctas += palabra_seleccionada.count(letra)         #aumenta el conteo de letras correctas segun las veces que aparezca
-        if letras_correctas == numero_letras:
+    rae_palabras_f1 = [palabra for palabra in rae_palabras if len(palabra) == numero_letras]   # filtro 1: palabras que contengan el mismo numero de letras
+    f1 = ''.join(rae_palabras_f1)
+    frecuencia_letras1 = Counter(f1)
+    letras_ordenadas = sorted(frecuencia_letras1.items(), key=lambda x: x[1], reverse=True)
+
+    # Creo la primera lista de letras a elegir en base a el orden de uso en filtro 1
+    primer_set = [letra for letra, frecuencia in letras_ordenadas]
+
+    #print(primer_set)
+    
+    intentos_realizados = 0
+    nletras_correctas = 0
+    letras_correctas_lista = []
+    letras_incorrectas_lista = []
+    indice = 0  
+
+    while indice < len(primer_set):        # Bucle while para controlar el índice (disclaimer: esto lo he tenido que hacer porque iterar la lista primer_set directamente, ha sido imposible)
+
+        letra = primer_set[indice]                          # Siempre elige la primera letra
+        intentos_realizados += 1
+
+        if palabra_seleccionada.count(letra) > 0:
+            nletras_correctas += palabra_seleccionada.count(letra)
+            letras_correctas_lista.append(letra)
+            rae_palabras_f1 = [palabra for palabra in rae_palabras_f1 if all(letra in palabra for letra in letras_correctas_lista)]        # filtro 2.1: en base a las palabras que contienen la letra adivinada
+            #print(f'Letras correctas {letras_correctas_lista}')
+
+        if palabra_seleccionada.count(letra) == 0:
+            letras_incorrectas_lista.append(letra)
+            rae_palabras_f1 = [palabra for palabra in rae_palabras_f1 if letra not in palabra]                                              # filtro 2.2: en base a las palabras que no contienen la letra fallada
+            #print(f'Letras incorrectas : {letras_incorrectas_lista}')
+
+        # Actualiza primer_set de letras eligibles
+        primer_set = [letra for letra, frecuencia in sorted(Counter(''.join(rae_palabras_f1)).items(), key=lambda x: x[1], reverse=True)]      #Ordena la lista de letras en base a la frecuencia de uso a partir de los filtros 2
+        primer_set = [letra for letra in primer_set if letra not in letras_correctas_lista]                                                    # Elimina de posibles letras las que ya ha adivinado previamente
+        #print(primer_set)
+
+
+        if nletras_correctas == numero_letras:
             break
 
 
