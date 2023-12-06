ccfa511e94beb587e455115610bfb4fd705d3c71
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2023-12-06 12:38:31 +0100

    entregables python

---
+++ Alumnos/ES/Jorge_Dominguez/python/entregables/s1.py
@@ -0,0 +1,5 @@
+#1.Crea un archivo Python que muestre por consola "¡Hola, Mundo!"
+print('Hello World')
+#2.Ahora modifícalo para que muestre "¡Hola, [Nombre]" donde el nombre es el valor (str) de una variable
+nombre:str = 'Jorge'
+print(f'Hello {nombre}')
