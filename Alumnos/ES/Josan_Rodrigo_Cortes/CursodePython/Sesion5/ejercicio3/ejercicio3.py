b648ee00c44161474c6962874ae2e62d963e6f66
Jose Antonio Rodrigo Cortes <josanrc.rcsl@gmail.com>
2023-12-09 19:04:52 +0100

    091223 1904

--- Alumnos/ES/Josan_Rodrigo_Cortes/CursodePython/Sesion5/ejercicio3/ejercicio3.py
+++ Alumnos/ES/Josan_Rodrigo_Cortes/CursodePython/Sesion5/ejercicio3/ejercicio3.py
@@ -7,23 +7,39 @@ Camión'''
 from ejercicio2 import Automovil
 	
 class Moto(Automovil):
-	
- 
-	def __init__(self, marca,modelo,color,matricula,id_seguro,titular,velocidad,ruedas,tipo):
-        #Aquí haríamos uso del constructor de super
-		super().__init__(self, marca,modelo,color,matricula,id_seguro,titular,velocidad,ruedas,tipo)
-            self.marca=marca
-            self.modelo=modelo
-            self.color=color
-            self.matricula=matricula
-            self.id_seguro=id_seguro
-            self.titular=titular
-            self.velocidad=velocidad
-            self.ruedas = ruedas 
-            self.tipo=tipo
- 
-	def tumbarEnLasCurvas():
-            print("La moto esta tomando una curva y va casi rozando el suelo")
+    def __init__(self, marca, modelo, color, matricula, id_seguro, titular, velocidad, ruedas, tipodemoto):
+        # Llamamos al constructor de la clase base (Automovil)
+        super().__init__(marca, modelo, color, matricula, id_seguro, titular, velocidad, ruedas)
+        # Atributo adicional específico de la clase Moto
+        self.tipo_moto = tipodemoto
+
+    # Métodos adicionales o sobrescritos específicos de la clase Moto
+    def realizar_acrobacia(self):
+        print(f"La moto ha realizado una acrobacia.")
 			
-moto=Moto(tipo="Carretera")
-print(moto)
+
+
+class Camion(Automovil):
+    def __init__(self, marca, modelo, color, matricula, id_seguro, titular, velocidad, ruedas, capacidad_carga):
+        # Llamamos al constructor de la clase base (Automovil)
+        super().__init__(marca, modelo, color, matricula, id_seguro, titular, velocidad, ruedas)
+        # Atributo adicional específico de la clase Camion
+        self.capacidad_carga = capacidad_carga
+
+    # Métodos adicionales o sobrescritos específicos de la clase Camion
+    def cargar_mercancia(self):
+        print(f"El camión ha cargado mercancía.")
+
+
+
+
+# Crear una instancia de Moto
+moto_ejemplo = Moto(marca="Honda", modelo="CBR", color="Rojo", matricula="ABC123", id_seguro="12345",
+                    titular="Juan", velocidad=0, ruedas=2, tipo_moto="Deportiva")
+
+# Acceder a atributos y métodos de la clase base
+print(moto_ejemplo.velocidad)  # Imprimir velocidad
+moto_ejemplo.arrancar()         # Llamar al método arrancar de la clase base
+
+# Llamar a métodos específicos de la clase Moto
+moto_ejemplo.realizar_acrobacia()
