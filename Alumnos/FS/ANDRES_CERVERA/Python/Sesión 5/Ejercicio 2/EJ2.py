4e7c6abe4301068453239918ec2600a249d63b7c
andrescervera <andreescerveraa99@gmail.com>
2024-01-01 13:39:48 +0100

    commit

--- Alumnos/FS/ANDRES_CERVERA/Python/Sesión 5/Ejercicio 2/EJ2.py
+++ Alumnos/FS/ANDRES_CERVERA/Python/Sesión 5/Ejercicio 2/EJ2.py
@@ -2,32 +2,50 @@
 
 class Automovil:
     
-    def __init__(auto, marca, modelo):
-        auto.marca = marca
-        auto.modelo = modelo
-        auto.velocidad = 0
-        auto.encendido = False
+    def __init__(init, marca, modelo):
+        init.marca = marca
+        init.modelo = modelo
+        init.velocidad = 0
+        init.encendido = False
         
-    def arrancar(auto):
-        if not auto.encendido:
-            auto.encendido = True
+    def arrancar(init):
+        if not init.encendido:
+            init.encendido = True
             print(f'Coche arrancado')
-            print(f'La velocidad del coche es {auto.velocidad} KM/H')
         else:
             print(f'Ya estaba arrancado')
-            print(f'La velocidad del coche es {auto.velocidad} KM/H')
+            print(f'La velocidad del coche es {init.velocidad} KM/H')
             
-    def acelerar(auto, velocidad):
-       auto.velocidad = auto.velocidad + velocidad
-       print(f'Acelerando...\n'
-             f'La velocidad del coche es {auto.velocidad} KM/H')
+    def acelerar(init, velocidad):
+        if init.encendido == True:
+            init.velocidad += velocidad
+            print(f'Acelerando...\n'
+                    f'La velocidad del coche es {init.velocidad} KM/H')
+        else:
+            print('El coche está apagado')
     
-    def acelerar(auto, velocidad):
-       auto.velocidad = auto.velocidad + velocidad
-       print(f'Acelerando...\n'
-             f'La velocidad del coche es {auto.velocidad} KM/H')
+    def frenar(init, velocidad):
+        if init.encendido == True:
+            init.velocidad -= velocidad
+            print(f'Frenando...\n'
+                    f'La velocidad del coche es {init.velocidad} KM/H')
+        else:
+            print('El coche está apagado')
+    
+    def parar(init):
+        if init.encendido:
+            init.encendido = False
+            init.velocidad = 0
+            print(f'Coche parado')
+            print(f'La velocidad del coche es {init.velocidad} KM/H')
+        else:
+            print(f'Ya estaba apagado')
             
         
 coche = Automovil(marca = 'Alfa Romeo', modelo= 'Giulietta')
+
 coche.arrancar()
 coche.acelerar(10)
+coche.frenar(5)
+coche.parar()
+
