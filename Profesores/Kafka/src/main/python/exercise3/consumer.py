21f8fbbbd28a74ba66da377cfe3dcc17c46662f0
Pedro Nieto <pedro.nieto@onlyfy.com>
2024-01-02 10:10:23 +0100

    Revert "Kafka Ejercicio 3"
    This reverts commit 5dab6c456abf5278cdb12e457e7a53554a935ae1.

--- Profesores/Kafka/src/main/python/exercise3/consumer.py
+++ Profesores/Kafka/src/main/python/exercise3/consumer.py
@@ -15,12 +15,12 @@ def read_ccloud_config(config_file):
     return conf
 
 
-props = read_ccloud_config("client.properties")
+props = read_ccloud_config("exercise3/client.properties")
 props["group.id"] = "python-group-1"
 props["auto.offset.reset"] = "earliest"
 
 consumer = Consumer(props)
-consumer.subscribe(["topic_python2"])
+consumer.subscribe(["topic_python1"])
 
 try:
     while True:
