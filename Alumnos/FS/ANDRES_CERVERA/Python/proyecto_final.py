8ba663c7fe5e566f2db25b601a3016d8b45a0897
andrescervera <andreescerveraa99@gmail.com>
2024-01-03 16:25:26 +0100

    proyecto_final

--- Alumnos/FS/ANDRES_CERVERA/Python/proyecto_final.py
+++ Alumnos/FS/ANDRES_CERVERA/Python/proyecto_final.py
@@ -75,77 +75,134 @@ def añadir_alumno():
     
     alumnos.append(nuevo_alumno)
     print(f'Alumno añadido correctamente.')
-    main()
+    otra_operacion()
 
+#funcion eliminar alumno
 def eliminar_alumno():
     print(f'Escriba el NIF del alumno a eliminar:\n')
     NIF = input('NIF: ')
     for alumno in alumnos:
         if(NIF == alumno['NIF']):
             alumnos.remove(alumno) 
-            print(f'Alumno eliminado correctamente')  
+            print(f'Alumno eliminado correctamente')
+            otra_operacion()
+            return
     print(f'No se ha encontrado ningún alumno con ese NIF')
     otra_operacion()
-    
+
+#funcion actualizar datos por NIF   
 def actualizar_datos():
     print(f'Escriba el NIF del alumno a actualizar:\n')
     NIF = input('NIF: ')
     for alumno in alumnos:
         if(alumno['NIF'] == NIF):
-            print(f'{alumno}\n')
+            print(f'Rellene los datos para actualizar el alumno:\n')
+            NIF_actualizado = input('NIF: ')
+            Nombre_actualizado = input('Nombre: ')
+            Apellidos_actualizado = input('Apellidos: ')
+            Telefono_actualizado = input('Telefono: ')
+            Email_actualizado = input('Email: ')
+            Calificacion_actualizado = 'NC'
             
+            alumno['NIF'] = NIF_actualizado
+            alumno['Nombre'] = Nombre_actualizado
+            alumno['Apellidos'] = Apellidos_actualizado
+            alumno['Telefono'] = Telefono_actualizado
+            alumno['Email'] = Email_actualizado
+            alumno['Calificacion'] = Calificacion_actualizado
+            
+            print(f'Alumno actualizado correctamente:\n')
+            otra_operacion()
+            return
+    print(f'No se ha encontrado ningún alumno con ese NIF')       
+    otra_operacion()
+          
+#funcion mostrar datos por NIF                       
 def mostrar_por_nif():
     print(f'Escriba el NIF del alumno a mostrar:\n')
     NIF = input('NIF: ')
     for alumno in alumnos:
         if(alumno['NIF'] == NIF):
             print(f'{alumno}\n')
-            
+            otra_operacion()
+            return
+    print(f'No se ha encontrado ningún alumno con ese NIF')   
+    otra_operacion()
+
+#funcion mostrar datos por email            
 def mostrar_por_email():
     print(f'Escriba el Email del alumno a mostrar:\n')
     email = input('Email: ')
     for alumno in alumnos:
         if(alumno['Email'] == email):
             print(f'{alumno}\n')
-            
+            otra_operacion()
+            return
+    print(f'No se ha encontrado ningún alumno con ese Email')
+    otra_operacion()
+
+#funcion aprobar alumno por NIF            
 def aprobar_alumno():
     print(f'Escriba el NIF del alumno a aprobar:\n')
     NIF = input('NIF: ')
     for alumno in alumnos:
         if(NIF == alumno['NIF']):
             alumno['Calificacion'] = 'Aprobado'   
+            print(f'Alumno aprobado')
+            otra_operacion()
             return
+    print(f'No se ha encontrado ningún alumno con ese NIF')
+    otra_operacion()
 
+#funcion suspender alumno por NIF
 def suspender_alumno():
     print(f'Escriba el NIF del alumno a suspender:\n')
     NIF = input('NIF: ')
     for alumno in alumnos:
         if(NIF == alumno['NIF']):
             alumno['Calificacion'] = 'Suspendido'   
+            print(f'Alumno suspendido')
+            otra_operacion()
             return
+    print(f'No se ha encontrado ningún alumno con ese NIF')
+    otra_operacion()
 
+#funcion mostrar aprobados
 def mostrar_aprobados():
     for alumno in alumnos:
         if(alumno['Calificacion'] == 'Aprobado'):
             print(f'{alumno}\n')
+            otra_operacion()
+            return
+    print(f'No hay ningún alumno aprobado')
+    otra_operacion()
 
+#funcion mostrar suspendidos
 def mostrar_suspendidos():
     for alumno in alumnos:
         if(alumno['Calificacion'] == 'Suspendido'):
             print(f'{alumno}\n')
+            otra_operacion()
+            return
+    print(f'No hay ningún alumno suspendido')
+    otra_operacion()
                    
 #funcion listar alumno
 def listar_todos():
     for alumno in alumnos:
         print(f'Alumno {alumno}: {alumnos}\n')
+        otra_operacion()
+        return
+    print(f'No hay alumnos registrados')
     otra_operacion()
 
+#funcion para poder solicitar mas operaciones al acabar una
 def otra_operacion():
     print(f'¿Desea realizar otra operación?\n'
           f'1 --> Sí\n'
           f'X -- Salir')
     numero = input('')
-    if (numero == 1):
+    if (numero == '1'):
         main()
     elif (numero.upper() == 'X'):
         salir()
@@ -153,6 +210,7 @@ def otra_operacion():
         print(f'Caracter no válido')
         otra_operacion()
 
+#funcion para abandonar el programa
 def salir():
     print(f'¡Nos vemos!')
     exit()
