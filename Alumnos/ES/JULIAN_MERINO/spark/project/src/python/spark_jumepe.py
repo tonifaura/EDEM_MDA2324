c990cff4f7c03188e06694f5d6422778f8365931
Julián Merino <62222144+jumepre@users.noreply.github.com>
2024-01-15 09:43:22 +0100

    Spark - update
    Python script updated - paths corrected, now it works fine.

--- Alumnos/ES/JULIAN_MERINO/spark/project/src/python/spark_jumepe.py
+++ Alumnos/ES/JULIAN_MERINO/spark/project/src/python/spark_jumepe.py
@@ -1,10 +1,29 @@
-utmb_18DF = spark.read.option("header", "true").option("delimiter", ";").csv("utmb_18.csv")
-utmb_19DF = spark.read.option("header", "true").option("delimiter", ";").csv("utmb_19.csv")
-utmb_21DF = spark.read.option("header", "true").option("delimiter", ";").csv("utmb_21.csv")
-utmb_22DF = spark.read.option("header", "true").option("delimiter", ";").csv("utmb_22.csv")
-utmb_23DF = spark.read.option("header", "true").option("delimiter", ";").csv("utmb_23.csv")
+# Initialise Spark Session
+from pyspark.sql import SparkSession
+
+# Create the session
+spark = SparkSession \
+        .builder \
+        .appName("Window Partitioning") \
+        .master("local[*]") \
+        .getOrCreate()
+
+spark.version
+spark
+
+# For Pandas conversion optimization
+spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+
+# Import sql functions
+from pyspark.sql.functions import *
+
+utmb_18DF = spark.read.option("header", "true").option("delimiter", ";").csv("/opt/project/resources/utmb_18.csv")
+utmb_19DF = spark.read.option("header", "true").option("delimiter", ";").csv("/opt/project/resources/utmb_19.csv")
+utmb_21DF = spark.read.option("header", "true").option("delimiter", ";").csv("/opt/project/resources/utmb_21.csv")
+utmb_22DF = spark.read.option("header", "true").option("delimiter", ";").csv("/opt/project/resources/utmb_22.csv")
+utmb_23DF = spark.read.option("header", "true").option("delimiter", ";").csv("/opt/project/resources/utmb_23.csv")
 utmb_18DF.show()
 utmb_19DF.show()
 utmb_21DF.show()
 utmb_22DF.show()
-utmb_23DF.show()
+utmb_23DF.show(50)
