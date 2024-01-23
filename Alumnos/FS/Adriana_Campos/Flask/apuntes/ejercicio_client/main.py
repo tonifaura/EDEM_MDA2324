df0b61e7aee611242a1a627beba654277a12d968
iasinDev <iasin.dev@gmail.com>
2024-01-24 00:42:51 +0100

    Delete Alumnos/FS/Adriana_Campos/Flask directory
    It gives error in cloning
    
    $ git clone git@github.com:iasinDev/EDEM_MDA2324.git
    Cloning into 'EDEM_MDA2324'...
    remote: Enumerating objects: 12505, done.
    remote: Counting objects: 100% (561/561), done.
    remote: Compressing objects: 100% (353/353), done.
    remote: Total 12505 (delta 212), reused 495 (delta 186), pack-reused 11944
    Receiving objects: 100% (12505/12505), 75.63 MiB | 15.06 MiB/s, done.
    Resolving deltas: 100% (4878/4878), done.
    error: invalid path 'Alumnos/FS/Adriana_Campos/Flask/entregable /Dockerfile'
    fatal: unable to checkout working tree
    warning: Clone succeeded, but checkout failed.
    You can inspect what was checked out with 'git status'
    and retry with 'git restore --source=HEAD :/'

--- Alumnos/FS/Adriana_Campos/Flask/apuntes/ejercicio_client/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-# obtenemos por el terminal lo que hay en la API
-#el get es para trearte lo que esta en la api
-
-ret=requests.get("https://pokeapi.co/api/v2/pokemon/1")
-print(ret)
-print(ret.json())
